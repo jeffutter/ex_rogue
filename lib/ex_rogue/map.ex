da5e3c03d063c62b98ce4378b8149634f33fb97a
Jeffery Utter <jeff@jeffutter.com>
2020-03-21 08:09:51 -0500

    Add walls

--- lib/ex_rogue/map.ex
+++ lib/ex_rogue/map.ex
@@ -1,7 +1,23 @@
 defmodule ExRogue.Map do
+  defstruct width: 0, height: 0, map: []
+
+  @type t :: %__MODULE__{width: integer, height: integer, map: list(list(any))}
+
+  defmodule Tile.Wall do
+    defstruct id: 0, position: {0, 0}
+    @type t :: %__MODULE__{id: integer, position: {integer, integer}}
+  end
+
+  defmodule Tile.Room do
+    defstruct id: 0, position: {0, 0}
+    @type t :: %__MODULE__{id: integer, position: {integer, integer}}
+  end
+
+  alias Tile.{Room, Wall}
+
   def build(options \\ []) do
-    width = Keyword.get(options, :width, 10)
-    height = Keyword.get(options, :height, 10)
+    width = Keyword.get(options, :width, 50)
+    height = Keyword.get(options, :height, 30)
 
     width
     |> new(height)
@@ -10,70 +26,102 @@ defmodule ExRogue.Map do
   end
 
   def new(width, height) do
-    for _ <- 0..width do
+    map =
       for _ <- 0..height do
-        :e
+        for _ <- 0..width do
+          nil
+        end
       end
-    end
+
+    %__MODULE__{width: width, height: height, map: map}
   end
 
-  def place_rooms(map, options \\ []) do
+  def place_rooms(%__MODULE__{} = map, options \\ []) do
     rooms = Keyword.get(options, :rooms, 4)
 
     Enum.reduce(1..rooms, map, fn _, map -> place_room(map, options) end)
   end
 
   def place_room(
-        map,
+        %__MODULE__{} = map,
         options \\ [],
         id \\ System.unique_integer([:positive, :monotonic]),
         attempts \\ 10
       ) do
-    min_size = Keyword.get(options, :min_size, 4)
-    max_size = Keyword.get(options, :max_size, 6)
+    min_size = Keyword.get(options, :min_size, 8)
+    max_size = Keyword.get(options, :max_size, 60)
     size = Enum.random(min_size..max_size)
     do_place_room(map, size, id, attempts)
   end
 
-  def fill_space(map) do
-    x_max = length(List.first(map)) - 1
-    y_max = length(map) - 1
+  def fill_space(%__MODULE__{width: width, height: height} = map) do
+    x_max = width - 1
+    y_max = height - 1
 
-    for x <- 0..x_max do
+    data =
       for y <- 0..y_max do
-        map
-        |> get(x, y)
-        |> case do
-          :e -> :e
-          v -> v
+        for x <- 0..x_max do
+          map
+          |> get({x, y})
+          |> case do
+            nil -> nil
+            v -> v
+          end
         end
       end
-    end
+
+    %__MODULE__{map | map: data}
   end
 
-  defp do_place_room(map, _size, _, 0), do: map
+  defp do_place_room(%__MODULE__{} = map, _size, _, 0), do: map
 
-  defp do_place_room(map, size, id, attempts) do
-    width = Enum.random(1..floor(size / 2))
-    height = floor(size / width)
+  defp do_place_room(%__MODULE__{width: map_width, height: map_height} = map, size, id, attempts) do
+    IO.inspect(size, label: "Size")
+    IO.inspect(attempts, label: "Attempts")
+    # +2 for walls
+    width = Enum.random(1..floor(size / 2)) + 2
+    # +2 for walls
+    height = floor(size / width) + 2
 
-    x_max = length(List.first(map)) - width
-    y_max = length(map) - height
+    x_max = map_width - width
+    y_max = map_height - height
 
     left = Enum.random(0..x_max)
     right = left + width - 1
     top = Enum.random(0..y_max)
     bottom = top + height - 1
 
+    IO.inspect({width, height}, label: "WH")
+    IO.inspect({left, right, top, bottom}, label: "Cords")
+
     left..right
     |> Enum.reduce_while({:ok, map}, fn x, {:ok, map} ->
       top..bottom
       |> Enum.reduce_while({:ok, map}, fn y, {:ok, map} ->
-        {old_val, map} = get_and_update(map, x, y, {:r, id})
+        tile =
+          case {x, y} do
+            {x, _} when x == left ->
+              %Wall{id: id, position: {x, y}}
 
-        case old_val do
-          {:r, _} -> {:halt, :collision}
-          _ -> {:cont, {:ok, map}}
+            {x, _} when x == right ->
+              %Wall{id: id, position: {x, y}}
+
+            {_, y} when y == top ->
+              %Wall{id: id, position: {x, y}}
+
+            {_, y} when y == bottom ->
+              %Wall{id: id, position: {x, y}}
+
+            _ ->
+              %Room{id: id, position: {x, y}}
+          end
+
+        {old_val, map} = get_and_update(map, {x, y}, tile)
+
+        case {old_val, tile} do
+          {nil, _} -> {:cont, {:ok, map}}
+          {%Wall{}, %Wall{}} -> {:cont, {:ok, map}}
+          _ -> {:halt, :collision}
         end
       end)
       |> case do
@@ -87,11 +135,43 @@ defmodule ExRogue.Map do
     end
   end
 
-  defp get(map, x, y) do
+  defp get(%__MODULE__{map: map}, {x, y}) do
     get_in(map, [Access.at(y), Access.at(x)])
   end
 
-  defp get_and_update(map, x, y, value) do
-    get_and_update_in(map, [Access.at(y), Access.at(x)], &{&1, value})
+  defp get_and_update(%__MODULE__{map: data} = map, {x, y}, value) do
+    {old_val, data} = get_and_update_in(data, [Access.at(y), Access.at(x)], &{&1, value})
+    {old_val, %__MODULE__{map | map: data}}
+  end
+end
+
+defimpl Inspect, for: ExRogue.Map do
+  import Inspect.Algebra
+  alias ExRogue.Map
+  alias ExRogue.Map.Tile.{Room, Wall}
+
+  def inspect(%Map{width: width, map: map}, _opts) do
+    data =
+      [
+        String.duplicate("_", width + 2),
+        for row <- map do
+          col_string =
+            for col <- row, into: "" do
+              case col do
+                nil -> " "
+                %Room{} -> "R"
+                %Wall{} -> "W"
+              end
+            end
+
+          Enum.join(["|", col_string, "|"])
+        end,
+        String.duplicate("_", width + 2)
+      ]
+      |> List.flatten()
+
+    data
+    |> Enum.intersperse(break("\n"))
+    |> concat()
   end
 end
